4aeba7492b1d92eca244b3f3ce1eaf21b891090c
Grant Lemons <grantlemons@aol.com>
2025-03-29 09:40:13 -0600

    add p2p node creation to host

--- p2p/host/host.go
+++ p2p/host/host.go
@@ -1,20 +1,41 @@
 package host
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
-	"os/signal"
 
 	"github.com/b-sharman/pear/p2p"
+	"github.com/libp2p/go-libp2p"
+	peerstore "github.com/libp2p/go-libp2p/core/peer"
 )
 
 func Start(roomid string) error {
+	// start a libp2p node that listens on a random local TCP port
+	node, err := libp2p.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// print the node's PeerInfo in multiaddr format
+	peerInfo := peerstore.AddrInfo{
+		ID:    node.ID(),
+		Addrs: node.Addrs(),
+	}
+	addrs, err := peerstore.AddrInfoToP2pAddrs(&peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("libp2p node address:", addrs[0])
+	b := bytes.NewBufferString(addrs[0].String())
+
 	u, _ := url.Parse(p2p.ServerUrl)
 	u = u.JoinPath("register", roomid)
-	resp, err := http.Post(u.String(), "", nil)
+	resp, err := http.Post(u.String(), "", b)
+
 	if err != nil {
 		return err
 	}
@@ -22,24 +43,10 @@ func Start(roomid string) error {
 		return fmt.Errorf("name server returned %s\n", resp.Status)
 	}
 
-	cmd := exec.Command("tmux", "new", "-s", roomid)
+	cmd := exec.Command("man", "cat")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("tmux error: %s", err.Error())
-	}
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Kill)
-
-	<-c
-	killCmd := exec.Command("tmux", "kill-session", "-t", roomid)
-
-	if err := killCmd.Run(); err != nil {
-		return fmt.Errorf("tmux kill error: %s", err.Error())
-	}
 
 	return nil
 }
