2fd0517bf992737c43893d3fb85228674de5fe20
Grant Lemons <grantlemons@aol.com>
2025-03-29 12:26:55 -0600

    add relay to multiaddr

--- p2p/host/host.go
+++ p2p/host/host.go
@@ -19,7 +19,10 @@ import (
 
 func Start(roomid string, exitSignal chan int) error {
 	// start a libp2p node that listens on a random local TCP port
-	node, err := libp2p.New()
+	node, err := libp2p.New(
+		libp2p.EnableRelay(),
+		libp2p.EnableHolePunching(),
+	)
 	if err != nil {
 		panic(err)
 	}
@@ -65,7 +68,7 @@ func registerRoom(port string, id peerstore.ID, roomid string) error {
 	}
 
 	// create addr string from public ip, port, and id
-	b := bytes.NewBufferString(fmt.Sprintf("/ip4/%s/tcp/%v/p2p/%s", strings.TrimSpace(string(ip)), port, id))
+	b := bytes.NewBufferString(fmt.Sprintf("/dns4/pear-programming.fly.dev/tcp/%v/ip4/%s/tcp/%v/p2p/%s", 1337, strings.TrimSpace(string(ip)), port, id))
 
 	u, _ := url.Parse(p2p.ServerUrl)
 	u = u.JoinPath("register", roomid)
