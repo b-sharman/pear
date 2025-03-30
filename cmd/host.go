58effb07a89f67b41d40a728fa2a4caed3947a1b
Lukas Werner <lukas.m.werner@outlook.com>
2025-03-29 21:47:27 -0600

    basic udp conn

--- cmd/host.go
+++ cmd/host.go
@@ -1,26 +1,43 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
+
+var hostname string
 
 // hostCmd represents the host command
 var hostCmd = &cobra.Command{
 	Use:   "host",
 	Short: "Host a Pear session from your terminal",
-	Run: host,
+	Run:   host,
 }
 
 func init() {
 	rootCmd.AddCommand(hostCmd)
+	hostCmd.Flags().StringVarP(&hostname, "dial", "d", "pear-programming-dial.fly.dev:3000", "")
 }
 
 func host(cmd *cobra.Command, args []string) {
 	fmt.Println("host called")
+	conn, err := net.Dial("udp", hostname)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	shellCmd := exec.Command("tmux", "attach", "-t", "pear")
+	shellCmd.Stdin = conn
+	shellCmd.Stdout = conn
+	err = shellCmd.Run()
+	if err != nil {
+		log.Println(err.Error())
+	}
 }
--- cmd/serverLaunch.go
+++ cmd/serverLaunch.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -13,9 +16,11 @@ var serverLaunchCmd = &cobra.Command{
 	Use:   "server-launch",
 	Short: "Launch an instance of the central server",
 	Run: func(cmd *cobra.Command, args []string) {
-		const addr = ":8080"
-		fmt.Println("listening on", addr)
-		listenAndServe(addr)
+		const httpAddr = ":8080"
+		const udpAddr = "0.0.0.0:4000"
+		fmt.Println("listening on", httpAddr)
+		go udpRelayServer(udpAddr)
+		listenAndServeHTTP(httpAddr)
 	},
 	// Hidden: true,
 }
@@ -24,7 +29,7 @@ func init() {
 	rootCmd.AddCommand(serverLaunchCmd)
 }
 
-func listenAndServe(addr string) {
+func listenAndServeHTTP(addr string) {
 	http.HandleFunc("GET /api/lookup", func(w http.ResponseWriter, r *http.Request) {
 		if !r.URL.Query().Has("roomid") {
 			w.WriteHeader(http.StatusBadRequest)
@@ -37,5 +42,44 @@ func listenAndServe(addr string) {
 		fmt.Fprint(w, multiaddr)
 	})
 
-        log.Fatalln(http.ListenAndServe(addr, nil))
+	log.Fatalln(http.ListenAndServe(addr, nil))
+}
+func udpRelayServer(addr string) {
+	udpAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	buffer := make([]byte, 65535)
+
+	for {
+		n, remoteAddr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Println("Error reading from UDP:", err)
+			continue
+		}
+		if err == nil {
+			go func() {
+				for {
+					_, err := conn.WriteTo([]byte("j"), remoteAddr)
+					if err != nil {
+						log.Println("Error writing to UDP:", err)
+					}
+					time.Sleep(time.Second * 4)
+				}
+			}()
+		}
+
+		// Write the received data directly to stdout
+		_, _ = os.Stdout.Write(buffer[:n])
+		if err != nil {
+			log.Println("Error writing to stdout:", err)
+		}
+
+		// Optionally log the source address to stderr
+		fmt.Fprintf(os.Stderr, "Received %d bytes from %s\n", n, remoteAddr)
+	}
 }
